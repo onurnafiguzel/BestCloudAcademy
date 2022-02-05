1da16da6a24e4c3b414616ad7693a16a070df79f
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2022-02-05 18:30:29 +0300

    Controller and Newtonsoft added.

---
+++ BestCloudAcademy.WebAPI/Controllers/TempreatureController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestCloudAcademy.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TempreatureController : ControllerBase
+    {
+    }
+}
